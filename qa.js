a31a55b73dafefb220f2e30ca64bfdc589f75d12
ddiazag1 <ddiazag1@gmail.com>
2026-09-23 05:14:25 -0700

    Author SOTW Ch 18-27; cards for Greece, Rome, Persia

--- qa.js
+++ qa.js
@@ -8,10 +8,12 @@ require('./curriculum-data.js');
 require('./original-lessons.js');
 require('./original-lessons-2.js');
 require('./original-lessons-3.js');
+require('./original-lessons-4.js');
 require('./knowledge-cards.js');
 require('./knowledge-cards-2.js');
 require('./knowledge-cards-3.js');
 require('./knowledge-cards-4.js');
+require('./knowledge-cards-5.js');
 require('./sequence.js');
 
 const S = window.SEQ, KC = window.KC, L = {};
