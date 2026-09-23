42642e4d1026793cc681ae294585e820e650fff0
ddiazag1 <ddiazag1@gmail.com>
2026-09-22 21:11:43 -0700

    Author SOTW Ch 5-8 and BFSU B-3, D-2

--- qa.js
+++ qa.js
@@ -6,8 +6,10 @@
 global.window = {};
 require('./curriculum-data.js');
 require('./original-lessons.js');
+require('./original-lessons-2.js');
 require('./knowledge-cards.js');
 require('./knowledge-cards-2.js');
+require('./knowledge-cards-3.js');
 require('./sequence.js');
 
 const S = window.SEQ, KC = window.KC, L = {};
