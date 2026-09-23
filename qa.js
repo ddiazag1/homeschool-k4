c18ab8ce31ef8628f92cda97c946418a5eff6bbd
ddiazag1 <ddiazag1@gmail.com>
2026-09-22 21:17:43 -0700

    Complete Body strand; author SOTW Ch 11-16

--- qa.js
+++ qa.js
@@ -7,9 +7,11 @@ global.window = {};
 require('./curriculum-data.js');
 require('./original-lessons.js');
 require('./original-lessons-2.js');
+require('./original-lessons-3.js');
 require('./knowledge-cards.js');
 require('./knowledge-cards-2.js');
 require('./knowledge-cards-3.js');
+require('./knowledge-cards-4.js');
 require('./sequence.js');
 
 const S = window.SEQ, KC = window.KC, L = {};
